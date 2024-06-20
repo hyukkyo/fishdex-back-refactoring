1394c6f36d005440757a960bd64945608587220b
hyukkyo <shk5644@gmail.com>
2024-06-21 00:21:01 +0900

    업로드 오류로 원상복구

--- nodejs/src/services/imageService.js
+++ nodejs/src/services/imageService.js
@@ -19,35 +19,35 @@ exports.uploadImage = async (
   );
 
   // 뱃지 알고리즘
-  switch (category) {
-    case "Black Sea Sprat":
-      await userRepository.addFishByUsercode(usercode, "blackseasprat");
-      break;
-    case "Trout":
-      await userRepository.addFishByUsercode(usercode, "trout");
-      break;
-    case "Striped Red Mullet":
-      await userRepository.addFishByUsercode(usercode, "stripedredmullet");
-      break;
-    case "Shrimp":
-      await userRepository.addFishByUsercode(usercode, "shrimp");
-      break;
-    case "Sea Bass":
-      await userRepository.addFishByUsercode(usercode, "seabass");
-      break;
-    case "Red Sea Bream":
-      await userRepository.addFishByUsercode(usercode, "redseabream");
-      break;
-    case "Red Mullet":
-      await userRepository.addFishByUsercode(usercode, "redmullet");
-      break;
-    case "Horse Mackerel":
-      await userRepository.addFishByUsercode(usercode, "horsemackerel");
-      break;
-    case "Gilt-Head Bream":
-      await userRepository.addFishByUsercode(usercode, "giltheadbream");
-      break;
-  }
+  // switch (category) {
+  //   case "Black Sea Sprat":
+  //     await userRepository.addFishByUsercode(usercode, "blackseasprat");
+  //     break;
+  //   case "Trout":
+  //     await userRepository.addFishByUsercode(usercode, "trout");
+  //     break;
+  //   case "Striped Red Mullet":
+  //     await userRepository.addFishByUsercode(usercode, "stripedredmullet");
+  //     break;
+  //   case "Shrimp":
+  //     await userRepository.addFishByUsercode(usercode, "shrimp");
+  //     break;
+  //   case "Sea Bass":
+  //     await userRepository.addFishByUsercode(usercode, "seabass");
+  //     break;
+  //   case "Red Sea Bream":
+  //     await userRepository.addFishByUsercode(usercode, "redseabream");
+  //     break;
+  //   case "Red Mullet":
+  //     await userRepository.addFishByUsercode(usercode, "redmullet");
+  //     break;
+  //   case "Horse Mackerel":
+  //     await userRepository.addFishByUsercode(usercode, "horsemackerel");
+  //     break;
+  //   case "Gilt-Head Bream":
+  //     await userRepository.addFishByUsercode(usercode, "giltheadbream");
+  //     break;
+  // }
 
   return { message: "이미지 등록 완료" };
 };
